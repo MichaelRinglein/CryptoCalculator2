fca95c9c4c46598a069c021cfffec2e92e19f31d
MichaelRinglein <michaelringlein@gmail.com>
2018-01-27 13:54:36 +0200

    custom columns

---
+++ src/dropDownOptions.js
@@ -0,0 +1,301 @@
+
+<Item label="0x" value="ZRX" />
+<Item label="ATMChain" value="ATM" />
+<Item label="Achain" value="ACT" />
+<Item label="AdEx" value="ADX" />
+<Item label="adToken" value="ADT" />
+<Item label="aelf" value="ELF" />
+<Item label="Aeon" value="AEON" />
+<Item label="Aeternity" value="AE" />
+<Item label="Agoras Tokens" value="AGRS" />
+<Item label="Agrello" value="DLT" />
+<Item label="Aion" value="AION" />
+<Item label="AirSwap" value="AST" />
+<Item label="Ambrosus" value="AMB" />
+<Item label="AppCoins" value="APPC" />
+<Item label="Aragon" value="ANT" />
+<Item label="Ardor" value="ARDR" />
+<Item label="Ark" value="ARK" />
+<Item label="Asch" value="XAS" />
+<Item label="Augur" value="REP" />
+<Item label="BLOCKv" value="VEE" />
+<Item label="Bancor" value="BNT" />
+<Item label="Basic Attention Token" value="BAT" />
+<Item label="Bibox Token" value="BIX" />
+<Item label="Binance Coin" value="BNB" />
+<Item label="BitBay" value="BAY" />
+<Item label="BitClave" value="CAT" />
+<Item label="BitConnect" value="BCC" />
+<Item label="bitCNY" value="BITCNY" />
+<Item label="BitDegree" value="BDG" />
+<Item label="BitShares" value="BTS" />
+<Item label="Bitcoin" value="BTC" />
+<Item label="Bitcoin Cash" value="BCH" />
+<Item label="Bitcoin Gold" value="BTG" />
+<Item label="BitcoinDark" value="BTCD" />
+<Item label="Bitcore" value="BTX" />
+<Item label="BlackCoin" value="BLK" />
+<Item label="BlockMason Credit Protocol" value="BCPT" />
+<Item label="Blocknet" value="BLOCK" />
+<Item label="Blocktix" value="TIX" />
+<Item label="Bloom" value="BLT" />
+<Item label="Bread" value="BRD" />
+<Item label="BridgeCoin" value="BCO" />
+<Item label="Burst" value="BURST" />
+<Item label="Byteball Bytes" value="GBYTE" />
+<Item label="Bytecoin" value="BCN" />
+<Item label="Bytom" value="BTM" />
+<Item label="COSS" value="COSS" />
+<Item label="CanYaCoin" value="CAN" />
+<Item label="Cappasity" value="CAPP" />
+<Item label="Cardano" value="ADA" />
+<Item label="Centra" value="CTR" />
+<Item label="ChainLink" value="LINK" />
+<Item label="Cindicator" value="CND" />
+<Item label="Civic" value="CVC" />
+<Item label="CloakCoin" value="CLOAK" />
+<Item label="Cobinhood" value="COB" />
+<Item label="Cofound.it" value="CFI" />
+<Item label="CoinDash" value="CDT" />
+<Item label="Counterparty" value="XCP" />
+<Item label="Covesting" value="COV" />
+<Item label="Crown" value="CRW" />
+<Item label="Cryptonex" value="CNX" />
+<Item label="CyberMiles" value="CMT" />
+<Item label="DECENT" value="DCT" />
+<Item label="DEW" value="DEW" />
+<Item label="Dash" value="DASH" />
+<Item label="Datum" value="DAT" />
+<Item label="Decentraland" value="MANA" />
+<Item label="Decision Token" value="HST" />
+<Item label="Decred" value="DCR" />
+<Item label="DeepBrain Chain" value="DBC" />
+<Item label="DeepOnion" value="ONION" />
+<Item label="Delphy" value="DPY" />
+<Item label="Dent" value="DENT" />
+<Item label="Dentacoin" value="DCN" />
+<Item label="Diamond" value="DMD" />
+<Item label="DigiByte" value="DGB" />
+<Item label="DigitalNote" value="XDN" />
+<Item label="DigixDAO" value="DGD" />
+<Item label="Dimecoin" value="DIME" />
+<Item label="district0x" value="DNT" />
+<Item label="Divi" value="DIVX" />
+<Item label="Dogecoin" value="DOGE" />
+<Item label="DomRaider" value="DRT" />
+<Item label="Dragonchain" value="DRGN" />
+<Item label="Dynamic Trading Rights" value="DTR" />
+<Item label="E-coin" value="ECN" />
+<Item label="ECC" value="ECC" />
+<Item label="EOS" value="EOS" />
+<Item label="ETHLend" value="LEND" />
+<Item label="Edgeless" value="EDG" />
+<Item label="Eidoo" value="EDO" />
+<Item label="Einsteinium" value="EMC2" />
+<Item label="Elastic" value="XEL" />
+<Item label="Electra" value="ECA" />
+<Item label="Electroneum" value="ETN" />
+<Item label="Emercoin" value="EMC" />
+<Item label="EncrypGen" value="DNA" />
+<Item label="Enigma" value="ENG" />
+<Item label="Enjin Coin" value="ENJ" />
+<Item label="Ethereum" value="ETH" />
+<Item label="Ethereum Classic" value="ETC" />
+<Item label="Etherparty" value="FUEL" />
+<Item label="Ethos" value="ETHOS" />
+<Item label="Everex" value="EVX" />
+<Item label="Expanse" value="EXP" />
+<Item label="Experience Points" value="XP" />
+<Item label="Factom" value="FCT" />
+<Item label="FairCoin" value="FAIR" />
+<Item label="Feathercoin" value="FTC" />
+<Item label="FirstBlood" value="1ST" />
+<Item label="Flash" value="FLASH" />
+<Item label="FunFair" value="FUN" />
+<Item label="GXShares" value="GXS" />
+<Item label="Game.com" value="GTC" />
+<Item label="GameCredits" value="GAME" />
+<Item label="Gas" value="GAS" />
+<Item label="Genesis Vision" value="GVT" />
+<Item label="Gifto" value="GTO" />
+<Item label="Gnosis" value="GNO" />
+<Item label="Golem" value="GNT" />
+<Item label="Grid+" value="GRID" />
+<Item label="GridCoin" value="GRC" />
+<Item label="Groestlcoin" value="GRS" />
+<Item label="Gulden" value="NLG" />
+<Item label="HTMLCOIN" value="HTML" />
+<Item label="HelloGold" value="HGT" />
+<Item label="HempCoin" value="THC" />
+<Item label="High Performance Blockchain" value="HPB" />
+<Item label="Hive" value="HVN" />
+<Item label="Hshare" value="HSR" />
+<Item label="Humaniq" value="HMQ" />
+<Item label="I/O Coin" value="IOC" />
+<Item label="ICON" value="ICX" />
+<Item label="iExec RLC" value="RLC" />
+<Item label="INS Ecosystem" value="INS" />
+<Item label="ION" value="ION" />
+<Item label="IOTA" value="MIOTA" />
+<Item label="Iconomi" value="ICN" />
+<Item label="Ink" value="INK" />
+<Item label="Internet Node Token" value="INT" />
+<Item label="IoT Chain" value="ITC" />
+<Item label="iXledger" value="IXT" />
+<Item label="Jinn" value="JINN" />
+<Item label="Karma" value="KRM" />
+<Item label="Kin" value="KIN" />
+<Item label="Komodo" value="KMD" />
+<Item label="KuCoin Shares" value="KCS" />
+<Item label="Kyber Network" value="KNC" />
+<Item label="LAToken" value="LA" />
+<Item label="LBRY Credits" value="LBC" />
+<Item label="Lamden" value="TAU" />
+<Item label="Lisk" value="LSK" />
+<Item label="Litecoin" value="LTC" />
+<Item label="Loopring" value="LRC" />
+<Item label="Lunyr" value="LUN" />
+<Item label="Lykke" value="LKK" />
+<Item label="MaidSafeCoin" value="MAID" />
+<Item label="Maker" value="MKR" />
+<Item label="Matchpool" value="GUP" />
+<Item label="MediBloc" value="MED" />
+<Item label="MediShares" value="MDS" />
+<Item label="Melon" value="MLN" />
+<Item label="Mercury" value="MER" />
+<Item label="Metal" value="MTL" />
+<Item label="Metaverse ETP" value="ETP" />
+<Item label="MinexCoin" value="MNX" />
+<Item label="MobileGo" value="MGO" />
+<Item label="Modum" value="MOD" />
+<Item label="Moeda Loyalty Points" value="MDA" />
+<Item label="MonaCoin" value="MONA" />
+<Item label="Monaco" value="MCO" />
+<Item label="Monero" value="XMR" />
+<Item label="Monetha" value="MTH" />
+<Item label="Mooncoin" value="MOON" />
+<Item label="Mothership" value="MSP" />
+<Item label="NAGA" value="NGC" />
+<Item label="NAV Coin" value="NAV" />
+<Item label="NEM" value="XEM" />
+<Item label="NEO" value="NEO" />
+<Item label="NVO" value="NVST" />
+<Item label="Namecoin" value="NMC" />
+<Item label="Neblio" value="NEBL" />
+<Item label="Nebulas" value="NAS" />
+<Item label="Neumark" value="NEU" />
+<Item label="Nexus" value="NXS" />
+<Item label="Nimiq" value="NET" />
+<Item label="NuShares" value="NSR" />
+<Item label="Nuls" value="NULS" />
+<Item label="Numeraire" value="NMR" />
+<Item label="Nxt" value="NXT" />
+<Item label="OmiseGO" value="OMG" />
+<Item label="Oyster" value="PRL" />
+<Item label="PACcoin" value="PAC" />
+<Item label="PIVX" value="PIVX" />
+<Item label="Particl" value="PART" />
+<Item label="Pascal Coin" value="PASC" />
+<Item label="PayPie" value="PPP" />
+<Item label="Paypex" value="PAYX" />
+<Item label="Peercoin" value="PPC" />
+<Item label="Peerplays" value="PPY" />
+<Item label="Pepe Cash" value="PEPECASH" />
+<Item label="Pillar" value="PLR" />
+<Item label="Po.et" value="POE" />
+<Item label="Populous" value="PPT" />
+<Item label="PotCoin" value="POT" />
+<Item label="Power Ledger" value="POWR" />
+<Item label="Presearch" value="PRS" />
+<Item label="Propy" value="PRO" />
+<Item label="Pura" value="PURA" />
+<Item label="QASH" value="QASH" />
+<Item label="QLINK" value="QLC" />
+<Item label="Qtum" value="QTUM" />
+<Item label="Quantstamp" value="QSP" />
+<Item label="Quantum Resistant Ledger" value="QRL" />
+<Item label="RChain" value="RHOC" />
+<Item label="RaiBlocks" value="XRB" />
+<Item label="Raiden Network Token" value="RDN" />
+<Item label="Red Pulse" value="RPX" />
+<Item label="ReddCoin" value="RDD" />
+<Item label="Request Network" value="REQ" />
+<Item label="Revain" value="R" />
+<Item label="Ripio Credit Network" value="RCN" />
+<Item label="Ripple" value="XRP" />
+<Item label="Rise" value="RISE" />
+<Item label="Rivetz" value="RVT" />
+<Item label="SALT" value="SALT" />
+<Item label="SHIELD" value="XSH" />
+<Item label="SIBCoin" value="SIB" />
+<Item label="SIRIN LABS Token" value="SRN" />
+<Item label="SONM" value="SNM" />
+<Item label="Safe Exchange Coin" value="SAFEX" />
+<Item label="SaluS" value="SLS" />
+<Item label="Santiment Network Token" value="SAN" />
+<Item label="Selfkey" value="KEY" />
+<Item label="Shift" value="SHIFT" />
+<Item label="Siacoin" value="SC" />
+<Item label="Simple Token" value="OST" />
+<Item label="SingularDTV" value="SNGLS" />
+<Item label="Skycoin" value="SKY" />
+<Item label="SmartCash" value="SMART" />
+<Item label="SolarCoin" value="SLR" />
+<Item label="SophiaTX" value="SPHTX" />
+<Item label="SpankChain" value="SPANK" />
+<Item label="Spectrecoin" value="XSPEC" />
+<Item label="Status" value="SNT" />
+<Item label="Steem" value="STEEM" />
+<Item label="Steem Dollars" value="SBD" />
+<Item label="Stellar" value="XLM" />
+<Item label="Storj" value="STORJ" />
+<Item label="Storm" value="STORM" />
+<Item label="Stratis" value="STRAT" />
+<Item label="Streamr DATAcoin" value="DATA" />
+<Item label="Substratum" value="SUB" />
+<Item label="SunContract" value="SNC" />
+<Item label="SuperNET" value="UNITY" />
+<Item label="Swarm City" value="SWT" />
+<Item label="Synereo" value="AMP" />
+<Item label="Syscoin" value="SYS" />
+<Item label="TRON" value="TRX" />
+<Item label="TaaS" value="TAAS" />
+<Item label="Telcoin" value="TEL" />
+<Item label="TenX" value="PAY" />
+<Item label="Tether" value="USDT" />
+<Item label="Theta Token" value="THETA" />
+<Item label="Tierion" value="TNT" />
+<Item label="Time New Bank" value="TNB" />
+<Item label="TokenCard" value="TKN" />
+<Item label="Trade Token" value="TIO" />
+<Item label="Triggers" value="TRIG" />
+<Item label="Trinity Network Credit" value="TNC" />
+<Item label="UTRUST" value="UTK" />
+<Item label="Ubiq" value="UBQ" />
+<Item label="Unikoin Gold" value="UKG" />
+<Item label="VIBE" value="VIBE" />
+<Item label="VeChain" value="VEN" />
+<Item label="Verge" value="XVG" />
+<Item label="VeriCoin" value="VRC" />
+<Item label="Veritaseum" value="VERI" />
+<Item label="Vertcoin" value="VTC" />
+<Item label="Viacoin" value="VIA" />
+<Item label="Viberate" value="VIB" />
+<Item label="Voxels" value="VOX" />
+<Item label="WAX" value="WAX" />
+<Item label="WaBi" value="WABI" />
+<Item label="Wagerr" value="WGR" />
+<Item label="Walton" value="WTC" />
+<Item label="Waves" value="WAVES" />
+<Item label="WeTrust" value="TRST" />
+<Item label="WhiteCoin" value="XWC" />
+<Item label="Wings" value="WINGS" />
+<Item label="Worldcore" value="WRC" />
+<Item label="XPlay" value="XPA" />
+<Item label="XTRABYTES" value="XBY" />
+<Item label="YOYOW" value="YOYOW" />
+<Item label="ZClassic" value="ZCL" />
+<Item label="ZCoin" value="XZC" />
+<Item label="Zcash" value="ZEC" />
+<Item label="ZenCash" value="ZEN" />
+<Item label="Zeusshield" value="ZSC" />
